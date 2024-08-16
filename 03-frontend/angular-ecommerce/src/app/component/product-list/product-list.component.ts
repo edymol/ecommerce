095798ff614d6bfdfb66b65bd8635b7c9879375b
Edy Molina <edymolinap@gmail.com>
2024-08-17 00:20:54 +0100

    feat: 4. Enhance ProductListComponent to read category id param

--- 03-frontend/angular-ecommerce/src/app/component/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/component/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
@@ -10,15 +11,27 @@ import {Product} from "../../common/product";
 export class ProductListComponent implements OnInit {
 
     products: Product[] = [];
-    constructor(private productService: ProductService) {
+    currentCategoryId: number = 1;
+    constructor(private productService: ProductService,
+                private router: ActivatedRoute,) {
     }
 
     ngOnInit(): void {
-        this.listProducts();
+        this.router.paramMap.subscribe(() => {
+            this.listProducts();
+        });
     }
+// check for id parameter availability
 
     private listProducts() {
-        this.productService.getProductList().subscribe(
+        const hasCategoryId: boolean = this.router.snapshot.paramMap.has('id');
+        if (hasCategoryId) {
+            // Read the category id and convert to a number using (+) on front of this
+            this.currentCategoryId = +this.router.snapshot.paramMap.get('id')!; // Use '!' to assert that the value is non-null
+        } else {
+            this.currentCategoryId = 1;  // Default category ID
+        }
+        this.productService.getProductList(this.currentCategoryId).subscribe(
             data => {
                 this.products = data;
             },
