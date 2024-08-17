f392c161e9aa358368d12080239fe88d1db55fe4
Edy Molina <edymolinap@gmail.com>
2024-08-17 13:02:12 +0100

    feat: dynamic categories now getting info from database

--- 02-backend/spring-boot-ecommerce/src/main/java/xyz/developers/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/xyz/developers/ecommerce/config/MyDataRestConfig.java
@@ -1,5 +1,9 @@
 package xyz.developers.ecommerce.config;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
@@ -7,26 +11,44 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import xyz.developers.ecommerce.entity.Product;
 import xyz.developers.ecommerce.entity.ProductCategory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    HttpMethod[] theUnsupportedActions = { HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE };
+    private final EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    private static final HttpMethod[] theUnsupportedActions = { HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE };
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
-
-        // disable POST, PUT, DELETE for product
+        // Disable HTTP methods for Product: POST, PUT, DELETE
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
-                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
-                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
-        // disable POST, PUT, DELETE for ProductCategory
-
+        // Disable HTTP methods for ProductCategory: POST, PUT, DELETE
         config.getExposureConfiguration()
                 .forDomainType(ProductCategory.class)
-                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
-                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
+        // Expose entity IDs for all JPA entities
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class<?>> entityClasses = new ArrayList<>();
 
+        for (EntityType<?> tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        config.exposeIdsFor(entityClasses.toArray(new Class[0]));
     }
 }
